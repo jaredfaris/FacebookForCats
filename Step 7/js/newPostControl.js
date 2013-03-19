d2408f134612439a3494815f90d85adf639f8533
Jared Faris <jfaris@gmail.com>
2013-03-18 22:21:08 -0400

    Added a delete collection cleanup event.

---
+++ Step 7/js/newPostControl.js
@@ -0,0 +1,86 @@
+function newPostControl(formContainerId){
+	var self = this;
+
+	this.$formContainer = $('#' + formContainerId);
+
+	// setup a channel to publish to
+	this.newPostChannel = postal.channel("posts", "newPost");
+	
+	// setup a view model that contains the images to display and the text
+	this.viewModel = {
+		images: ko.observableArray([
+			{text: "Out of food", src: "./img/emptybowl.jpg"},
+			{text: "Ack", src: "./img/hairball.gif"},
+			{text: "Human toy object", src: "./img/hairtie.jpg"},
+			{text: "Center of attention", src: "./img/laptop.jpg"},
+			{text: "Lame inanimate toy", src: "./img/mouse.jpg"},
+		]),
+		title: ko.observable(""),
+		selectedImage: ko.observable(null)
+	}
+	
+	// put some events on our images for when they are clicked
+	for(im in this.viewModel.images()){
+		var viewModel = this.viewModel;
+		var image = this.viewModel.images()[im];
+		
+		// sets the collection's 'selectedImage' property to this image when clicked
+		image.selectImage = function() {
+			viewModel.selectedImage(this);
+		}
+		
+		// returns if this is the selected item
+		image.selected = function() {
+			return viewModel.selectedImage() == this;
+		}
+		
+		// returns a CSS class based on the selected state
+		image.selectedClass = function() {
+			if (this.selected())
+				return "selected";
+			else
+				return "";
+		}
+	}
+	
+	// return which image should be displayed
+	this.viewModel.imageSrcToDisplay = function() {
+		if (this.selectedImage()) 
+			return this.selectedImage().src;
+		else // or return a default image
+			return "./img/question.gif";
+	}
+	
+	// return the title that should be displayed
+	this.viewModel.titleToDisplay = function() {
+		if (this.title() && this.title().length > 0 ){
+			return this.title();
+		} else {
+			return "Mrow?";
+		}
+	}
+	
+	// setup the form post
+	this.viewModel.postUpdate = function() {
+		// capture the selected values
+		var imageSrc = this.imageSrcToDisplay();
+		var title = this.titleToDisplay();
+		// send out a message with these values
+		self.newPostChannel.publish({
+			src: imageSrc,
+			title: title
+		});
+		
+		// reset the form
+		this.title("");
+		this.selectedImage(null);
+		
+		// hide the form
+		self.$formContainer.hide();
+	}
+	
+
+	ko.applyBindings(this.viewModel, this.$formContainer[0]);	
+	
+	return this;
+}
---
+++ Step 7/js/scratchingPostControl.js
@@ -0,0 +1,91 @@
+function scratchingPostControl(scratchingPostContainerId){
+	var self = this;
+	
+	this.$postContainer = $('#' + scratchingPostContainerId);
+	
+	// create a model to store a post in
+	var postModel = Backbone.Model.extend({});
+
+	// we'll have a collection of these
+	var postsCollection = Backbone.Collection.extend({
+		model: postModel
+	});
+	
+	// let's define a child view for each item
+	var childView = Backbone.View.extend({
+		className: "post span6",
+		
+		events: {
+			"click" : "deleteMe"
+		},
+		
+		deleteMe: function(){
+			// tell the world we're going to be removed
+			// kind of hacky to pass the whole object but it works
+			postal.channel("posts", "removePost").publish(this.model);
+		
+			this.remove(); // removes this rendered view
+		},
+		
+		render: function() {
+			this.$el.html("<header>" + this.model.get('title')  + "</header><section><img src=\"" + this.model.get('src') + "\"></section>");
+		}
+	});
+	
+	// define a really basic view to represent the scratching post
+	var postsView = Backbone.View.extend({
+		className: "posts-container span6",
+		childViews: [], // a place to stash references to our children
+	
+		initialize: function() {
+			self = this;
+			
+			// add code to remove any deleted child views from our collection
+			postal.channel("posts", "removePost").subscribe(function(model){
+				console.log("I'm removing a model");
+				self.collection.remove(model);
+			});
+		},
+	
+		render: function() {
+		
+			// purge any existing views to prevent zombies
+			for(var i = 0; i < this.childViews.length; i++) {
+				childViews[i].remove();
+			}
+
+			this.$el.html('');
+			
+			for(var i = 0; i < this.collection.length; i++) {
+				item = this.collection.models[i];
+				
+				var child = new childView({model: item});
+				child.render()
+				
+				this.$el.append(child.el);
+				
+			}
+		}
+	});
+	
+	// let's initialize a view
+	var collection = new postsCollection([]);
+	var view = new postsView({
+		collection: collection
+	});
+	this.$postContainer.html(view.el); // and actually put it in our document
+	view.render();
+
+	// setup a channel to listen to
+	this.newPostChannel = postal.channel("posts", "newPost");
+	this.newPostChannel.subscribe(function(data){
+		// add something to the collection
+		collection.add({
+			title: data.title,
+			src: data.src
+		});
+		
+		// and then tell the view to re-render
+		view.render();
+	});
+}
